4dc79d669e8f9fd7ccb50ababf579aa94ab2e733
Hirad Roshandel <hirad@condos.ca>
2018-01-03 12:25:26 -0500

    added investment file

--- app.js
+++ app.js
@@ -48,49 +48,7 @@ app.use(function(err, req, res, next) {
 
 const remaining_ETH = 5.24504826+1.73064000;
 
-const my_investment = {
-  'QTUM':{
-    balance:14.44632619,
-    initial_buying:0.59999623,
-    currency: 'ETH',
-  },
-  'IOTA':{
-    balance:278.72100000,
-    initial_buying:1.56240000,
-    currency: 'ETH',
-  },
-  'ADA':{
-    balance:685.31457800,
-    initial_buying:0.40000126,
-    currency: 'ETH',
-  },
-  'XVG':{
-    balance:9990.51200000,
-    initial_buying:2.50000000,
-    currency: 'ETH',
-  },
-  'TRX':{
-    balance: 25874.10000000,
-    initial_buying: 1.53846000,
-    currency: 'ETH',
-  },
-  'XRP':{
-    balance: 0,
-    initial_buying: 0,
-    currency: 'ETH',
-  },
-  'VEN':{
-    balance: 129.87000000,
-    initial_buying: 0.35100000,
-    currency: 'ETH',
-  },
-  'XLM':{
-    balance: 549.45000000,
-    initial_buying: 0.31350000,
-    currency: 'ETH',
-  },
-
-};
+const my_investment = require('./investment');
 
 function generate_coin_data(data,my_investment) 
 {
